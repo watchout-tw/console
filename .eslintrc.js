1fbc28fa10a7d4e1769be6897dd3280638f5ed0b
chihao <chihaoyo@gmail.com>
2017-06-13 17:44:24 +0800

    Minor update

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,7 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // allow paren-less functions
-    'space-before-function-paren': 0,
+    'space-before-function-paren': 0, // allow paren-less functions
     'keyword-spacing': 0,
     'no-new': 0,
   }
