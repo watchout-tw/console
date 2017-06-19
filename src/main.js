64445f65b5b2356b63a74770cf4925f5a5c7f3ea
Wesley <wesley100002001@gmail.com>
2017-06-19 15:06:23 +0800

    Introduce Vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
@@ -13,6 +14,7 @@ Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }
