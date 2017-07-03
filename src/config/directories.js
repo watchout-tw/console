4b411fe5ac05a181f53d7657571e87b2656af020
chihao <chihaoyo@gmail.com>
2017-07-03 20:10:28 +0800

    Add more offline directories

--- src/config/directories.js
+++ src/config/directories.js
@@ -104,6 +104,60 @@ export default {
       {
         value: 'masters',
         label: '碩士'
+      },
+      {
+        value: 'bachelor',
+        label: '學士'
+      },
+      {
+        value: 'high_school',
+        label: '高中、高職'
+      },
+      {
+        value: 'middle_school',
+        label: '中學'
+      },
+      {
+        value: 'primary_school',
+        label: '小學'
+      },
+      {
+        value: 'other',
+        label: '其他'
+      }
+    ]
+  },
+  rep_term_change_type: {
+    id: 'rep_term_change_type',
+    values: [
+      {
+        value: 'assume_office_regular_election',
+        label: '應屆就任'
+      },
+      {
+        value: 'assume_office_by_election',
+        label: '補選就任'
+      },
+      {
+        value: 'leave_office',
+        label: '離任'
+      },
+      {
+        value: 'end_of_term',
+        label: '屆期結束'
+      }
+    ]
+  },
+  rep_term_office: {
+    id: 'rep_term_office',
+    values: [
+      {
+        value: 'speaker',
+        label: '院長'
+      },
+      {
+        value: 'deputy_speaker',
+        label: '副院長'
       }
     ]
   }
