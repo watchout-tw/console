dfecf60d4fc4dd44f93eab77fc9a64361f189093
Wesley <wesley100002001@gmail.com>
2017-07-18 13:47:50 +0800

    [bugfix] handle undefined case

--- src/components/EditorTable.vue
+++ src/components/EditorTable.vue
@@ -40,7 +40,7 @@ export default {
   },
   watch: {
     'isInitialized'(now) {
-      this.flags = this.rows.map(row => ({isEditing: false}))
+      this.flags = this.rows ? this.rows.map(row => ({isEditing: false})) : []
     }
   },
   methods: {
