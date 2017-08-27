4b753fb80a608b93f09b3fc43ac1da56570e7c1d
staristic <dnadnaacgt@gmail.com>
2017-08-27 19:59:41 +0800

    specific_topics patch, remove act_dirs id before send request

--- src/config/editors.js
+++ src/config/editors.js
@@ -688,6 +688,7 @@ export default {
           name: '修法方向',
           propListIsCalled: 'columns',
           preparerKeyIsAt: 'prop',
+          postPreparer: preparers.removeId,
           columns: [
             {
               id: 'name',
