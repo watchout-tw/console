b79172c5d8275a8294f13de4920c3e22785a17c2
Wesley <wesley100002001@gmail.com>
2017-12-26 12:47:52 +0800

    Rename figure type

--- src/config/options.js
+++ src/config/options.js
@@ -25,72 +25,72 @@ export default {
     bill_comp_bars: {
       value: 'bill_comp_bars',
       group: '提案比較',
-      label: '法案比較得分'
+      label: '提案成份比較'
     },
     bill_comp_radar: {
       value: 'bill_comp_radar',
       group: '提案比較',
-      label: '雷達'
+      label: '提案戰力分析'
     },
     bill_comp_compact_table: {
       value: 'bill_comp_compact_table',
       group: '提案比較',
-      label: '精簡列表'
+      label: '提案簡單比較'
     },
     bill_comp_full_table: {
       value: 'bill_comp_full_table',
       group: '提案比較',
-      label: '詳細列表'
+      label: '提案完整比較'
     },
     spsr_partisan_count: {
       value: 'spsr_partisan_count',
       group: '提案連署統計',
-      label: '政黨分組計數'
+      label: '提案與連署名單'
     },
     spsr_total_pct: {
       value: 'spsr_total_pct',
       group: '提案連署統計',
-      label: '全體委員比例'
+      label: '委員參與度甜甜圈'
     },
     spsr_partisan_pct: {
       value: 'spsr_partisan_pct',
       group: '提案連署統計',
-      label: '政黨比例'
+      label: '政黨參與度甜甜圈'
     },
     spsr_most_stats: {
       value: 'spsr_most_stats',
       group: '提案連署統計',
-      label: '最多連署'
+      label: '提案連署之最'
     },
     spsr_mesh: {
       value: 'spsr_mesh',
       group: '提案連署統計',
-      label: '委員網絡'
+      label: '提案連署人際網絡'
     },
     stmt_bar_map: {
       value: 'stmt_bar_map',
       group: '發言統計',
-      label: '政黨發言次數統計'
+      label: '發言計數器'
     },
     stmt_partisan_stats: {
       value: 'stmt_partisan_stats',
       group: '發言統計',
-      label: '政黨發言比例統計'
+      label: '政黨發言百分比'
     },
     stmt_bubbles: {
       value: 'stmt_bubbles',
       group: '發言統計',
-      label: '委員發言立場統計'
+      label: '委員立場甜甜圈'
     },
     stmt_not_found: {
       value: 'stmt_not_found',
       group: '發言統計',
-      label: '應發言而未發言的委員'
+      label: '誰沒說話'
     },
     stmt_playground: {
       value: 'stmt_playground',
       group: '發言統計',
-      label: '發言時間分布圖、互動圖表'
+      label: '發言遊戲區'
     }
   },
   figure_data_set_type: {
